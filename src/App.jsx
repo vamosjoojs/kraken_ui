c58686f8a4cc4e8e1f73e9666c19c6d2c994bebc
Davi Souza <davi.souza@upperds.com>
2022-03-26 15:32:24 -0300

    adicionando background

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import Navbar from './components/navbar'
 import Dashboard from "./pages/dashboard/Dashboard";
 import Twitch from './pages/twitch/Twitch';
 import Posts from './pages/posts/Posts';
+import Background from './assets/1553875.png';
 
 
 function App() {
+  const myStyle={
+    backgroundImage: `url(${Background})`,
+    height:'100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'repeat',
+};
+
   return (
     <Router>
       <Switch>
-        <div>
+        <div style={myStyle}>
           <Navbar name="Kraken"></Navbar>
           <Route exact path="/" component={Dashboard} />
           <Route path="/twitch" component={Twitch} />
